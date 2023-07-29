498f369ad7b532f3dcadac5801892f6010901750
Harsh2813 <prateektiwari.pk55@gmail.com>
2023-07-29 17:31:21 +0530

    added filter function

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
     let form = document.getElementById('addForm');
     let itemList = document.getElementById('items');
+    let filter = document.getElementById('filter');
 
     form.addEventListener('submit', addItem);
     itemList.addEventListener('click', removeItem);
+    filter.addEventListener('keyup', filterItems);
 
     function addItem(e){
         e.preventDefault();
         let newItem = document.getElementById('item').value;
+        let newItem2 = document.getElementById('item2').value;
         let li = document.createElement('li');
         li.className = 'list-group-item';
         li.appendChild(document.createTextNode(newItem));
+        li.appendChild(document.createTextNode(" "));
+        li.appendChild(document.createTextNode(newItem2));
 
         let deleteBtn = document.createElement('button');
         deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
@@ -24,12 +29,29 @@
         itemList.appendChild(li);
     }
     function removeItem(e){
-        if(e.target.classList.contains('delete')){
+        if(e.target.classList.contains('delete')){ // jo apna event hoga mtlb jispe bhi click karenge to jo wo target hoga uska class agar delete h to hi apan aage delete krenge mtlb jo X sign h sirf uska class delete h na ki pure li ka to li me X ko click karenge to fir are you sure ko yes krne ke baad delete hoga
             if(confirm('Are You Sure?')){
-                var li = e.target.parentElement;
-                itemList.removeChild(li);
+                var li = e.target.parentElement; // jo apne event ka target X uska parent element li me store kiye code me dekho X button ka parent element li h usi line ka to li me li ko rakh liye
+                itemList.removeChild(li);// fir item list ke sare child li h to jo current li liye h wo remove hoga item list se code dhyan se dekho li ke andar button tag h.
             }
         }
+    }
+   // Filter Items
+    function filterItems(e){
+        // convert text to lowercase
+        let text = e.target.value.toLowerCase(); // if we want to filter case sensitive so we can remove loLowerCase
+        // Get lis
+        let items = itemList.getElementsByTagName('li');
+        // Convert to an array
+        Array.from(items).forEach(function(item){
+        let itemName = item.textContent;
+        let itemName2 = item.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 || itemName2.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+        });
     }
     // let itemList = document.getElementById('items');
     // console.log(itemList.parentElement);
